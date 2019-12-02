73fa04d7d5a28d1f8041f3ddc525a11db5ce697d
Selchuk Karakus <selchuck.karakus@gmail.com>
2019-12-02 13:08:30 +0000

    tidyup and refactor code

--- js/index.js
+++ js/index.js
@@ -2,8 +2,20 @@ const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
 const cardsWrapper = document.querySelector('.cards-wrapper');
 const buttonWrapper = document.querySelector('.btn-wrapper');
 const mainButton = document.getElementById('start-game');
-
 const cards = [];
+
+// For each dataObject, create a new card and append it to the DOM
+function createCard() {
+  cards.forEach((card, i) => {
+    const positionFromLeft = i * 25;
+    const cardElement = document.createElement('div');
+    cardElement.setAttribute('data-value', card.value);
+    cardElement.classList.add('card', `${card.suit}-${card.value}`);
+    cardElement.style.left = `${positionFromLeft}px`;
+    cardsWrapper.append(cardElement);
+  });
+}
+
 function createCards() {
   // Create an array with objects containing the value and the suit of each card
   suits.forEach((suit) => {
@@ -15,44 +27,7 @@ function createCards() {
       cards.push(cardObject);
     }
   });
-
-  // For each dataObject, create a new card and append it to the DOM
-  cards.forEach((card, i) => {
-    const positionFromLeft = i * 25;
-    const cardElement = document.createElement('div');
-    cardElement.setAttribute('data-value', card.value);
-    cardElement.classList.add('card', `${card.suit}-${card.value}`);
-    cardElement.style.left = `${positionFromLeft}px`;
-    cardsWrapper.append(cardElement);
-  });
-}
-
-// Function to clear out the initial button and create new buttons to play the game.
-
-function createButtons() {
-  // Your Code
-  mainButton.remove();
-
-  const shuffle = document.createElement('BUTTON');
-  shuffle.setAttribute('id', 'start-game');
-  shuffle.setAttribute('class', 'btn btn-lg btn-secondary');
-  shuffle.innerHTML = 'Shuffle';
-  buttonWrapper.appendChild(shuffle);
-  shuffle.addEventListener('click', shuffleCards);
-
-  const showOrHide = document.createElement('BUTTON');
-  showOrHide.setAttribute('id', 'start-game');
-  showOrHide.setAttribute('class', 'btn btn-lg btn-secondary');
-  showOrHide.innerHTML = 'Show/Hide';
-  buttonWrapper.appendChild(showOrHide);
-  showOrHide.addEventListener('click', hide);
-
-  const magic = document.createElement('BUTTON');
-  magic.setAttribute('id', 'start-game');
-  magic.setAttribute('class', 'btn btn-lg btn-secondary');
-  magic.innerHTML = 'Magic';
-  buttonWrapper.appendChild(magic);
-  magic.addEventListener('click', hocusPocus);
+  createCard();
 }
 
 function shuffleCards() {
@@ -62,22 +37,46 @@ function shuffleCards() {
     cards[i] = cards[j];
     cards[j] = temp;
   }
-  createCards();
+  createCard();
 }
-// Function to start the game by clearing the wrapper, creating
-// and appending the buttons and all the cards to the DOM
+
 function hocusPocus() {
   cards.length = 0;
   createCards();
 }
 
+function hide() {
+  cardsWrapper.classList.toggle('hidden');
+}
+
+// Function to clear out the initial button and create new buttons to play the game.
+function createButtons() {
+  mainButton.remove();
+
+  const shuffle = document.createElement('BUTTON');
+  shuffle.setAttribute('id', 'start-game');
+  shuffle.setAttribute('class', 'btn btn-lg btn-secondary');
+  buttonWrapper.appendChild(shuffle).innerHTML = 'Shuffle';
+  shuffle.addEventListener('click', shuffleCards);
+
+  const showOrHide = document.createElement('BUTTON');
+  showOrHide.setAttribute('id', 'start-game');
+  showOrHide.setAttribute('class', 'btn btn-lg btn-secondary');
+  buttonWrapper.appendChild(showOrHide).innerHTML = 'Show/Hide';
+  showOrHide.addEventListener('click', hide);
+
+  const magic = document.createElement('BUTTON');
+  magic.setAttribute('id', 'start-game');
+  magic.setAttribute('class', 'btn btn-lg btn-secondary');
+  buttonWrapper.appendChild(magic).innerHTML = 'Magic';
+  magic.addEventListener('click', hocusPocus);
+}
+
+// Function to start the game by clearing the wrapper, creating
+// and appending the buttons and all the cards to the DOM
 function startGame() {
   createButtons();
   createCards();
-}
-
-function hide() {
-   cardsWrapper.classList.toggle('hidden');
 }
 
 mainButton.addEventListener('click', startGame);
